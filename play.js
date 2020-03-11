63b31f077b78ff5d0109cedfdf3f94461dc977f8
Dexter Chan <dexterchann@gmail.com>
2020-03-11 16:31:29 +0000

    Created separate module for TCP connection

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require("net");
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: "10.0.0.209",
-    port: 50541
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting...");
 connect();
