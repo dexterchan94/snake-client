aa9b79d283e5b2ba50d65c825b9108290927137d
Dexter Chan <dexterchann@gmail.com>
2020-03-11 16:56:46 +0000

    Added user input handling

--- play.js
+++ play.js
@@ -1,5 +1,27 @@
 const { connect } = require("./client");
 
+const handleUserInput = (key) => {
+
+  // exits game when ctrl + c is pressed
+  if (key === '\u0003') {
+    console.log(`Exiting...`);
+    process.exit();
+  }
+};
+
+const setupInput = (data) => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on('data', data);
+
+  return stdin;
+};
+
+
 console.log("Connecting...");
 connect();
+setupInput(handleUserInput);
 
